0251f39410126b5301bbadb36fc2173137f785f0
valde12 <113907060+valde12@users.noreply.github.com>
2024-04-19 10:51:58 +0200

    Update models.py

--- users_app/models.py
+++ users_app/models.py
@@ -1,13 +1,14 @@
 from django.db import models
-
+from encrypted_model_fields.fields import EncryptedCharField
 # Create your models here.
 class UserOrder(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
+    email = EncryptedCharField(max_length=100)
+    phone_number = EncryptedCharField(max_length=100, null="true")
     country = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
-    zipcode = models.CharField(max_length=100)
-    address1 = models.CharField(max_length=100)
-    address2 = models.CharField(max_length=100)
+    city = EncryptedCharField(max_length=100)
+    zipcode = EncryptedCharField(max_length=100)
+    address1 = EncryptedCharField(max_length=100)
+    address2 = EncryptedCharField(max_length=100)
     order_date = models.DateField()
